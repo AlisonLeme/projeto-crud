40b7a76740a8b55195e4e9b1fe1ce16479357da5
Alison Leme <alisonletos@protonmail.com>
2021-11-16 15:38:00 -0300

    adicinando feature de cadasto, edição, listagem e remoção de clientes

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,6 +1,14 @@
 const CustomersModel = require('../models/customers');
 const { crypto } = require('../utils/password');
 
+const defaltTitle = 'Cadasto de cliente';
+
+function index (req, res) {
+    res.render('register', {
+        title: defaltTitle
+    })
+}
+
 async function add(req, res) {
     const {
         name,
@@ -20,9 +28,87 @@ async function add(req, res) {
     })
 
     register.save();
-    res.send('cadastro realizado!');
+    res.render('register', {
+        title: defaltTitle,
+        message: 'Cadastro realizado com sucesso!'
+    })
+}
+
+async function listUsers(req, res) {
+    //indo no banco e buscando todos os clientes
+    const users = await CustomersModel.find(); // deixando vazio ele trás todos
+    
+
+    res.render('listUsers', {
+        title: 'Listagem de usuarios',
+        users: users
+    })
+}
+
+async function indexEdit(req, res) {
+    //console.log(req.query); // pegando os dados da url
+
+    const { id } = req.query;
+
+    // procurar esse usuario no banco usando um método do mongoose
+    // com esse método ele retorna o usuario, se não encontrar retorna null
+    const user = await CustomersModel.findById(id)
+
+    res.render('editUsers', {
+        title: 'Editar Usuário',
+        user: user,
+    })
+}
+
+async function edit(req, res) {
+    const {
+        name,
+        age,
+        email,
+    } = req.body
+
+    // pegando o formulário
+    //console.log(req.body);
+    // pegando o parametro passado na url
+    //console.log(req.params);
+
+    const { id } = req.params;
+
+    // procurar o usuario pelo ID e alterar os dados
+    const user = await CustomersModel.findById(id);
+
+    user.name = name;
+    user.age = age;
+    user.email = email;
+
+    user.save();
+
+    res.render('editUsers', {
+        title: 'Editar usuario',
+        user: 'user',
+        message: 'Usuario alterado com sucesso!'
+    })
+}
+
+async function remove(req, res) {
+    const { id } = req.params;
+
+    // método do mongoose para remover no BD
+    const remove = await CustomersModel.deleteOne({ _id: id });
+
+    // o método a cima retorna um objeto: deletedCount: 1 = ok  deletedCount: 0 = erro
+    if(remove.deletedCount == 1)
+    {
+        // apenas redirecionando para a rota listUsers
+        res.redirect('/listUsers')
+    }
 }
 
 module.exports = {
-    add
+    index,
+    add,
+    listUsers,
+    indexEdit,
+    edit,
+    remove,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,32 @@
 // importando o método router para exportar todas as rotas da aplicação
 const router = require('express').Router()
 
-const CustomersController= require('../controllers/customers')
+const CustomersController = require('../controllers/customers');
+const IndexController = require('../controllers/index');
 
 // rotas
-router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home'
-    })
-})
+router.get('/', IndexController.index);
 
-router.get('/register', (req, res) => {
-    res.render('register', {
-        title: 'Register'
-    })
-})
+// registro
+router.get('/register', CustomersController.index);
+
 
 // quando essa rota for chamada, vai executar essa função passando o req e o res
 // como essa rota é do tipo post, eu preciso que alguma coisa mande pra ca
-router.post('/register/add', CustomersController.add)
+router.post('/register', CustomersController.add)
+
+
+// Listar
+router.get('/listUsers', CustomersController.listUsers);
+
+
+// editar
+router.get('/editUsers', CustomersController.indexEdit); // renderizar a página de edir
+// mandando para uma rota com o id
+router.post('/editUsers:id', CustomersController.edit); // editar o usuario no BD
+
+
+// remover
+router.get('/removeUsers:id', CustomersController.remove)
 
 module.exports = router;
